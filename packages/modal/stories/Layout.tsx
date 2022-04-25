ff26cea80647094402974b4a518bb9fb4895d24c
Sergey Bondar <bondar.sn@greendatasoft.ru>
2022-04-25 14:11:09 +0300

    feat(modal): add useScrollbarWidth hook and observer for body styles

--- packages/modal/stories/Layout.tsx
+++ packages/modal/stories/Layout.tsx
@@ -1,7 +1,33 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import { useScrollbarWidth } from "react-use";
 
 import { ModalContextProvider } from "../src/ModalContext";
 
 export const Layout: FC = ({ children }) => {
-  return <ModalContextProvider>{children}</ModalContextProvider>;
+  const sbw = useScrollbarWidth();
+  const [isShowScrollbar, setIsShowScrollbar] = useState(false);
+
+  useEffect(() => {
+    const observer = new MutationObserver(([mutation]) => {
+      const isBodyOverflowHidden =
+        (mutation.target as HTMLBodyElement).style.overflow === "hidden";
+
+      setIsShowScrollbar(!isBodyOverflowHidden);
+    });
+    observer.observe(document.body, { attributes: true });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  return (
+    <div
+      style={{
+        paddingRight: isShowScrollbar ? "0px" : sbw + "px",
+      }}
+    >
+      <ModalContextProvider>{children}</ModalContextProvider>;
+    </div>
+  );
 };
